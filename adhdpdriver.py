0348c66aecbf262515fc053594a3be059d5cdc58
Ziyang Song <ziyang.song@gmx.de>
2021-01-05 16:30:30 +0100

    change parameter to test

--- adhdpdriver.py
+++ adhdpdriver.py
@@ -149,15 +149,15 @@ class Driver:
             #keras.layers.Conv2D(self.timeslip_size, (3, 3), padding='same', activation='elu', kernel_initializer='random_normal'),
             #keras.layers.Conv2D(self.timeslip_size, (3, 3), padding='same', activation='elu', kernel_initializer='random_normal'),
             #keras.layers.Conv2D(self.timeslip_size, (3, 3), padding='same', activation='elu', kernel_initializer='random_normal'),
-            #keras.layers.Conv2D(self.timeslip_size, (3, 3), padding='same', activation='elu', kernel_initializer='random_normal'),
-            keras.layers.Conv2D(self.timeslip_size, (3, 3), padding='same', activation='elu', kernel_initializer='glorot_normal'),
-            keras.layers.Conv2D(self.timeslip_size, (3, 3), padding='same', activation='elu', kernel_initializer='glorot_normal'),
-            keras.layers.Conv2D(self.timeslip_size, (3, 3), padding='same', activation='elu', kernel_initializer='glorot_normal'),
-            keras.layers.Conv2D(1, (3, 3), padding='same', activation='elu', kernel_initializer='random_normal'),
+            keras.layers.Conv2D(self.timeslip_size, (3, 3), padding='same', activation='elu', kernel_initializer='random_normal'),
+            keras.layers.Conv2D(self.timeslip_size, (3, 3), padding='same', activation='elu', kernel_initializer='glorot_uniform'),
+            keras.layers.Conv2D(self.timeslip_size, (3, 3), padding='same', activation='elu', kernel_initializer='glorot_uniform'),
+            keras.layers.Conv2D(self.timeslip_size, (3, 3), padding='same', activation='elu', kernel_initializer='glorot_uniform'),
+            keras.layers.Conv2D(1, (3, 3), padding='same', activation='elu', kernel_initializer='glorot_uniform'),
             keras.layers.Flatten(),
-            keras.layers.Dense(self.greedysnake.SIZE ** 2, activation = 'elu', kernel_initializer='random_normal'),
-            keras.layers.Dense((self.greedysnake.SIZE ** 2) // 2, activation = 'elu', kernel_initializer='random_normal'),
-            keras.layers.Dense(1, kernel_initializer='random_normal'),
+            keras.layers.Dense(self.greedysnake.SIZE ** 2, activation = 'elu', kernel_initializer='glorot_normal'),
+            keras.layers.Dense((self.greedysnake.SIZE ** 2) // 2, activation = 'elu', kernel_initializer='glorot_normal'),
+            keras.layers.Dense(1, kernel_initializer='glorot_normal'),
         ], name = 'critic')
 
         # actor layers
