a3945b488285f60fefbcdb396a5e6f3f1a49d99b
Ziyang Song <ziyang.song@gmx.de>
2021-01-11 18:28:38 +0100

    fix max Q

--- adhdp.py
+++ adhdp.py
@@ -43,7 +43,7 @@ class ADHDP(keras.Model):
             state_action = tf.concat([state, action], 1)
             q = self.critic(state_action)
             t = np.ones((self.batch_size, 1))              
-            t.fill(1.333333)                                             
+            t.fill(1.0)                                             
             actor_loss = self.loss(t, q)
         actor_grads = tape.gradient(actor_loss, self.actor.trainable_weights)
 
