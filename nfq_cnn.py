4ee1c4956546095f3db1cce4f3391ba4257f6510
root <root@vultr.guest>
2021-01-15 15:22:28 +0000

    Synchronize parameter changes

--- nfq_cnn.py
+++ nfq_cnn.py
@@ -245,7 +245,7 @@ class Driver:
                 if signal == Signal.HIT:
                     r = -1.
                     hits += 1
-                    # i = self.max_steps - 1    #  learn on hit
+                    i = self.max_steps - 1    #  learn on hit
                 elif signal == Signal.EAT:
                     r = 1.
                     eats += 1
